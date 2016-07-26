4c1725138b5dfe5ba3ec5a784874e573bd10d944
ericka1599 <erickasegura96@gmail.com>
2016-07-26 08:44:41 -0600

    avances

--- funciones.py
+++ funciones.py
@@ -1,6 +1,29 @@
-#Usar pickle para crear el archivo y luego leerlo?
+#Usar pickle para crear el archivo y luego leerlo? ¯ǀ⁄∕ ¯°˘ˆˉ•—|
 import random
 def palabra (lista):
 	palabra = open ("palabras.txt" , 'rb')
-	print (palabra)
+	pa = palabra.random
+	print (pa)
 
+def ahorcado (errores):
+	while errores < 7:
+		muere = False 
+	else:
+		muere = True
+		return "Perdiste"
+		des = int(input("1. Volver a jugar" '∕n' "2. Salir"))
+		if des = 1:
+			muere = True
+
+def intentos (errores):
+	if intento != letra
+
+def hombresito (errores):
+	pa1= " ———————"
+	pa2 = "ǀ" '\n'  "ǀ" '\n'  "ǀ" '\n'  "ǀ" 
+
+	 ———————
+	ǀ    (•˛•)
+	ǀ     \¦∕
+	ǀ
+	ǀ
