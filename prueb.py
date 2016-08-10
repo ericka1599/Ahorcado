d36daf1cb9b79fbaecff815f906c8420dbd277a4
Ericka Segura <erickasegura96@gmail.com>
2016-08-10 02:38:52 -0300

    ughh...

---
+++ prueb.py
@@ -0,0 +1,12 @@
+#palabra = "lola" .split()
+#palabral = palabra.split()
+
+#intento = "L"
+#intento = intento.lower()
+
+#print (palabra)
+
+
+for j in len(palabra):
+	if intento == palabral[j]:
+		guion[j] = intento
